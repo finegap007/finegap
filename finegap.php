7b3ec1e0ab800ff453fe08f18cd0969f5885027c
cubixsol <api.cubixsol@gmail.com>
2022-02-05 21:24:30 +0500

    First commit

---
+++ finegap.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://cubixsol.com/
+ * @since             1.0.0
+ * @package           cubixsol
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Finegap Web Development
+ * Plugin URI:        https://cubixsol.com/
+ * Description:       This is a custom plugin, Finegap :)  
+ * Version:           1.0.0
+ * Author:            Noman Ghaffar
+ * Author URI:        https://cubixsol.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       finegap
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
